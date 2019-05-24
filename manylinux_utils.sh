79d85cc5b32583cf7de94bbe37da3db6320d8b1c
Mir, A <mir-am@hotmail.com>
2019-05-24 16:37:46 +0430

    show the wheel's libs.

--- manylinux_utils.sh
+++ manylinux_utils.sh
@@ -43,12 +43,14 @@ function repair_wheelhouse {
         if [[ $whl == *none-any.whl ]]; then  # Pure Python wheel
             if [ "$in_dir" != "$out_dir" ]; then cp $whl $out_dir; fi
         else
+			auditwheel show $whl
             auditwheel repair $whl -w $out_dir/
             # Remove unfixed if writing into same directory
             if [ "$in_dir" == "$out_dir" ]; then rm $whl; fi
         fi
     done
     chmod -R a+rwX $out_dir
+	ls $out_dir -lh
 }
 
 function activate_ccache {
